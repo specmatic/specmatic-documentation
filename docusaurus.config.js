5e535d41ec5700c0d8f8cc36111b22f7df22ca60
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2026-01-31 17:07:13 +0530

    chore: make the default theme light theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,8 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       colorMode: {
-        respectPrefersColorScheme: true,
+        defaultMode: 'light',
+        respectPrefersColorScheme: false,
       },
       navbar: {
         title: 'Specmatic',
